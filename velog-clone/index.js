bef3c46e07b77a78b9de3239b5534c7606d6f4b6
seyeon <doll4586633@naver.com>
2021-10-26 19:03:20 +0900

    feat: Add slider, dark-mode

--- velog-clone/index.js
+++ velog-clone/index.js
@@ -1,6 +1,8 @@
 const history = document.querySelector(".menu__history");
 const selections = document.querySelectorAll(".selection");
 const mainPosts = document.querySelectorAll(".main__post");
+const arrowBack = document.querySelector(".arrow-back");
+const arrowForward = document.querySelector(".arrow-forward");
 
 history.addEventListener("click", () => {
   document.querySelector(".history__selection").classList.toggle("show");
@@ -31,3 +33,96 @@ mainPosts.forEach((mainPost) => {
     document.body.append(modal);
   });
 });
+function calcCount() {
+  const conWitdh = parseInt(
+    getComputedStyle(document.querySelector(".main__post-con")).width,
+    10
+  );
+  const postWitdh =
+    parseInt(
+      getComputedStyle(document.querySelector(".main__post")).width,
+      10
+    ) + 32;
+  return conWitdh / postWitdh;
+}
+(function slider() {
+  let bodyWidth = document.body.clientWidth;
+  let showingCount = calcCount();
+  let moving = false;
+  let start = 0;
+  let end = mainPosts.length;
+
+  window.addEventListener("resize", () => {
+    bodyWidth = document.body.clientWidth;
+    showingCount = calcCount();
+    if (end < showingCount) {
+      end = showingCount;
+      start = end - mainPosts.length;
+    }
+    slideMove("resize", bodyWidth, start);
+  });
+  arrowBack.addEventListener("click", () => {
+    if (moving || start >= 0) return;
+    moving = true;
+    start++;
+    end++;
+    slideMove("back", bodyWidth);
+    setTimeout(() => (moving = false), 500);
+  });
+  arrowForward.addEventListener("click", () => {
+    if (moving || end <= showingCount) return;
+    moving = true;
+    end--;
+    start--;
+    slideMove("forward", bodyWidth);
+    setTimeout(() => (moving = false), 500);
+  });
+})();
+function slideMove(direction, bodyWidth, start) {
+  const postLeft = `${getComputedStyle(mainPosts[0]).left}`;
+  const postWidth = `(${getComputedStyle(mainPosts[0]).width} + 32px)`;
+  switch (direction) {
+    case "back":
+      bodyWidth >= 1056
+        ? moveCalc(postLeft, "+", "22rem")
+        : moveCalc(postLeft, "+", postWidth);
+      break;
+    case "forward":
+      bodyWidth >= 1056
+        ? moveCalc(postLeft, "-", "22rem")
+        : moveCalc(postLeft, "-", postWidth);
+      break;
+    case "resize":
+      bodyWidth >= 1056
+        ? moveCalc(start, "*", "22rem")
+        : moveCalc(start, "*", postWidth);
+      break;
+  }
+}
+function moveCalc(operand1, operator, operand2) {
+  mainPosts.forEach((e) => {
+    e.style.left = `calc(${operand1} ${operator} ${operand2})`;
+  });
+}
+
+(function darkMode() {
+  let darkMode = localStorage.getItem("dark-mode");
+  let checked = document.querySelector(".header__mode-check");
+  const button = document.querySelector(".dark-mode-con");
+
+  window.addEventListener("load", () => {
+    if (darkMode) {
+      checked.checked = true;
+      document.documentElement.setAttribute("dark-mode", true);
+    }
+  });
+
+  button.addEventListener("click", () => {
+    darkMode
+      ? localStorage.removeItem("dark-mode")
+      : localStorage.setItem("dark-mode", "on");
+    document.documentElement.toggleAttribute("dark-mode");
+    darkMode = localStorage.getItem("dark-mode");
+    checked.checked = !checked.checked;
+  });
+})();
